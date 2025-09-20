6051346be3ae7fbd18be85217a55dfbccac042f3
JC DEMO <juliomoura2406@gmail.com>
2025-09-20 18:05:08 -0300

    OK

--- backend/old-server.js
+++ backend/old-server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
 
-const app = express();000000000000000000000000000000000000000000000000000
+const app = express();
 app.use(cors());
 app.use(express.json());
 
