a69e36e989fcf8f19245167f510a1851cb556eb8
JC DEMO <juliomoura2406@gmail.com>
2025-09-21 22:41:39 -0300

    URL

--- backend/supabaseClient.js
+++ backend/supabaseClient.js
@@ -1,6 +1,6 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = 'db.dixnalbvuonkqoycuoqv.supabase.co';
+const supabaseUrl = 'https://dixnalbvuonkqoycuoqv.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRpeG5hbGJ2dW9ua3FveWN1b3F2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1ODQ5NTQ4OSwiZXhwIjoyMDc0MDcxNDg5fQ.4ZQI9701OQ8Z4ZB6vC5kQnIIa2uba4lOY677Sd2ej4g'; // Use a chave SERVICE ROLE para backend!
 
 const supabase = createClient(supabaseUrl, supabaseKey);
